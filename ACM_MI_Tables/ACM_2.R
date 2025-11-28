3420f48ae8919f2f9dbad50b87d54c363df57a82
u-matt <Matthew.Bandura@communities.gov.uk>
2025-11-28 11:52:32 +0000

    cleaned up comments

--- ACM_MI_Tables/ACM_2.R
+++ ACM_MI_Tables/ACM_2.R
@@ -3,7 +3,7 @@ one_source_pro <- one_source_pro %>%
   mutate(Tenure = factor(Tenure),
          CombinedCategory = factor(CombinedCategory, levels = c('Remediation completed', 'Works complete awaiting building control signoff', 'Remediation started - cladding removed',
                                                                               'Remediation started', 'Plans in place', 'Responded with intent', 'Remediation plan unclear') ))
-
+#these are the row names used in the MI tables
 levels(one_source_pro$CombinedCategory) <- c('Completed Remediation', 'Works complete awaiting building control signoff', 'Remediation started - cladding removed', 'Remediation started', 'Remediation plans in place'
                              , 'Reported an intent to remediate', 'Remediation plan unclear')
 
@@ -24,7 +24,7 @@ ACM_2 <- list(one_source_pro %>% filter(Tenure == 'social') %>% count(CombinedCa
   # mutate(`Remediation Category` = factor(`Remediation Category`, levels = c('Completed Remediation', 'Works complete awaiting building control signoff', 'Remediation started - cladding removed', 'Remediation started', 'Remediation plans in place'
   #                                                                           , 'Reported an intent to remediate', 'Remediation plan unclear', 'Total buildings'))) %>%
   
-  #calculates the percentage of the total
+  #calculate the percentage of the total
   mutate(
     `Social sector residential Percentage (%)` = (`Social sector residential Number` / sum(`Social sector residential Number`)),
     `Private sector residential Percentage (%)` = (`Private sector residential Number` / sum(`Private sector residential Number`)),
@@ -33,7 +33,7 @@ ACM_2 <- list(one_source_pro %>% filter(Tenure == 'social') %>% count(CombinedCa
     `Publicly-owned buildings Percentage (%)` = (`Publicly-owned buildings Number` / sum(`Publicly-owned buildings Number`)),
     `Total: all tenures Percentage (%)` = (`Total: all tenures Number` / sum(`Total: all tenures Number`))) %>%
   
-  #reorders the columns
+  #reorder the columns
   select(
     `Remediation Category`,
     `Social sector residential Number`, `Social sector residential Percentage (%)`,
@@ -50,7 +50,7 @@ ACM_2 <- list(one_source_pro %>% filter(Tenure == 'social') %>% count(CombinedCa
   #order the rows correctly (according to the order specified in the factor levels)
   arrange(`Remediation Category`) %>% 
   
-  #creates the total row
+  #create the total row
   bind_rows(
     summarise(.,
               `Remediation Category` = "Total buildings",
