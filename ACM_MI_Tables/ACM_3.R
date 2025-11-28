6cd7ef7eb3b04f4b9e8b0695d0b3edaa8720fe41
u-matt <Matthew.Bandura@communities.gov.uk>
2025-11-28 11:51:47 +0000

    remove completed remediation and create ACM_3_2

--- ACM_MI_Tables/ACM_3.R
+++ ACM_MI_Tables/ACM_3.R
@@ -4,7 +4,10 @@ one_source_pro_vacants <- one_source_pro %>% filter(CombinedCategory != 'Complet
 #creating a new CombinedCategory variable which has the suffix - Vacant in the row names
 one_source_pro_vacants$CombinedCategory_Vacants <- one_source_pro_vacants$CombinedCategory
 
-levels(one_source_pro_vacants$CombinedCategory_Vacants) <- c('Completed Remediation', 'Works complete awaiting building control signoff - Vacant', 'Remediation started - cladding removed - Vacant', 'Remediation started - Vacant', 'Remediation plans in place - Vacant'
+#remove 'Completed Remediation' as a level as it's not needed
+one_source_pro_vacants <- droplevels(one_source_pro_vacants)
+
+levels(one_source_pro_vacants$CombinedCategory_Vacants) <- c('Works complete awaiting building control signoff - Vacant', 'Remediation started - cladding removed - Vacant', 'Remediation started - Vacant', 'Remediation plans in place - Vacant'
                                              , 'Reported an intent to remediate - Vacant', 'Remediation plan unclear - Vacant')
 
 
@@ -27,7 +30,7 @@ ACM_3_1 <- list(one_source_pro_vacants %>% filter(Tenure == 'social' & grepl('Va
   #order the rows correctly (according to the order specified in the factor levels)
   arrange(`Remediation Category`) %>% 
   
-  #creates the total row
+  #create the total row
   bind_rows(
     summarise(.,
               `Remediation Category` = "Total buildings",
@@ -38,4 +41,36 @@ ACM_3_1 <- list(one_source_pro_vacants %>% filter(Tenure == 'social' & grepl('Va
               `Publicly-owned buildings` = sum(`Publicly-owned buildings`),
     )
   )
+
+#creating ACM_3_2, showing remediation status and tenure for in-progress non vacant buildings
+ACM_3_2 <- list(one_source_pro_vacants %>% filter(Tenure == 'social' & is.na(Vacant)) %>% count(CombinedCategory, name  = 'Social sector residential'),
+              
+                one_source_pro_vacants %>% filter(Tenure == 'private residential'& is.na(Vacant)) %>% count(CombinedCategory, name  = 'Private sector residential'),
+              
+                one_source_pro_vacants %>% filter(Tenure == 'student' & is.na(Vacant)) %>% count(CombinedCategory, name  = 'Student accommodation'),
+              
+                one_source_pro_vacants %>% filter(Tenure == 'hotels' & is.na(Vacant)) %>% count(CombinedCategory, name  = 'Hotels'),
+              
+                one_source_pro_vacants %>% filter(Tenure == 'public' & is.na(Vacant)) %>% count(CombinedCategory, name  = 'Publicly-owned buildings')) %>% 
+  reduce(full_join, by = 'CombinedCategory') %>%
+  rename(`Remediation Category for occupied buildings` = `CombinedCategory` ) %>%
+  
+  #replace NA with 0
+  replace(is.na(.), 0) %>%
+  
+  #order the rows correctly (according to the order specified in the factor levels)
+  arrange(`Remediation Category for occupied buildings`) %>% 
+  
+  #create the total row
+  bind_rows(
+    summarise(.,
+              `Remediation Category for occupied buildings` = "Total buildings",
+              `Social sector residential` = sum(`Social sector residential`),
+              `Private sector residential` = sum(`Private sector residential`),
+              `Student accommodation` = sum(`Student accommodation`),
+              `Hotels` = sum(`Hotels`),
+              `Publicly-owned buildings` = sum(`Publicly-owned buildings`),
+    )
+  )
+
 
